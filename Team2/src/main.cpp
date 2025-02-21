f032602bc8ebf64ad814ed87e214c4238373f65d
KyanJeuring <kyanjeuring@gmail.com>
2025-02-21 10:18:29 +0100

    Added a note for converting the distance to cm

--- Team2/src/main.cpp
+++ Team2/src/main.cpp
@@ -113,6 +113,7 @@ long readSonarSensor()
     // Read the echo pin and calculate the distance
     long duration = pulseIn(SONAR_SENSOR_ECHO, HIGH);
     return duration * 0.034 / 2; // Convert duration to distance in cm by using the speed of sound (0.034 cm per microsecond)
+    // This does not convert to actual centimeters because of hardware compatibility issues or some other factors
 }
 
 // Drive with calibration
